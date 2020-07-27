ed02d68aab7644bed1793808439f769f019d0f0b
kukub <kukuborom@gmail.com>
2020-07-27 16:59:35 -0700

    grid

--- example/lib/profile.dart
+++ example/lib/profile.dart
@@ -2,8 +2,26 @@ import 'package:example/topdeck.dart';
 import 'package:flutter/material.dart';
 
 class Profilepage extends StatelessWidget {
+  List<Widget> texts = new List();
   @override
   Widget build(BuildContext context) {
+    texts.add(Text("First text baby"));
+    texts.add(Text("2nd text baby"));
+
+    texts.add(Text("3rd text baby"));
+
+    texts.add(Text("45thh text baby"));
+
+    texts.add(Text("7hjy text baby"));
+
+    texts.add(Text("kuku text baby"));
+
+    texts.add(Text("7gd text baby"));
+
+    texts.add(Text("69 text baby"));
+
+    texts.add(Text("sky text baby"));
+
     return Column(
       children: <Widget>[
         TopDeck(
@@ -13,32 +31,39 @@ class Profilepage extends StatelessWidget {
           knows: 3000,
         ),
         Container(
+          color: Colors.black,
           height: 56,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               Icon(
                 Icons.dashboard,
-                color: Colors.grey,
+                color: Colors.white,
                 size: 28,
               ),
               Icon(
                 Icons.image,
-                color: Colors.grey,
+                color: Colors.white,
                 size: 28,
               ),
               Icon(
                 Icons.play_circle_outline,
-                color: Colors.grey,
+                color: Colors.white,
                 size: 28,
               ),
             ],
           ),
         ),
         Container(
-          child: SingleChildScrollView(
-            child: GridView(gridDelegate: null),
-          ),
+          alignment: Alignment.topCenter,
+          height: 100,
+          child: GridView.count(
+              crossAxisCount: 2,
+              children: List.generate(texts.length, (index) {
+                return Image(
+                    image: NetworkImage(
+                        "https://i.ytimg.com/vi/msCO9CLZfT0/hqdefault.jpg"));
+              })),
         )
       ],
     );
