9bec40cdfba894ca4887ac9798d69d91e72978cf
kukub <kukuborom@gmail.com>
2020-07-30 06:25:19 -0700

    added animations

--- example/lib/Pages/Profile/tabbody/miniwidgets/ShowImage.dart
+++ example/lib/Pages/Profile/tabbody/miniwidgets/ShowImage.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
-class ShowImage extends StatelessWidget {
+class ShowImage extends StatefulWidget {
   final int index;
   final List<String> urls;
   ShowImage(this.index, {this.urls});
+
+  @override
+  _ShowImageState createState() => _ShowImageState();
+}
+
+class _ShowImageState extends State<ShowImage> {
+  Offset
+      animationStart; //Used in tween to set the starting position of Slide transition
+  Offset
+      animationEnd; //Used in Tween to set the ending position of the Slide Transition
+  int index;
+  @override
+  void initState() {
+    animationStart = Offset(0.0, 0.0);
+    animationEnd = Offset(0.0, 0.0);
+    super.initState();
+    index = widget.index;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,12 +32,28 @@ class ShowImage extends StatelessWidget {
           alignment: Alignment.center,
           children: [
             Center(
-              child: PhotoView(
-                imageProvider: NetworkImage(urls[index]),
-                basePosition: Alignment.center,
-                minScale: 0.2,
-                tightMode: true,
-                maxScale: 3.0,
+              child: AnimatedSwitcher(
+                transitionBuilder: (child, animation) {
+                  //Custom tween
+                  Tween<Offset> customTween =
+                      Tween<Offset>(begin: animationStart, end: animationEnd);
+
+                  //Custom animation
+                  Animation<Offset> custom = customTween.animate(animation);
+                  return SlideTransition(
+                    position: custom,
+                    child: child,
+                  );
+                },
+                duration: Duration(milliseconds: 200),
+                child: PhotoView(
+                  key: ValueKey(index),
+                  imageProvider: NetworkImage(widget.urls[index]),
+                  basePosition: Alignment.center,
+                  minScale: 0.2,
+                  tightMode: true,
+                  maxScale: 3.0,
+                ),
               ),
             ),
             Row(
@@ -29,7 +64,7 @@ class ShowImage extends StatelessWidget {
                     onPressed: index <= 0 ? null : () => prevImagee(context)),
                 IconButton(
                     icon: Icon(Icons.arrow_forward_ios),
-                    onPressed: index >= urls.length - 1
+                    onPressed: index >= widget.urls.length - 1
                         ? null
                         : () => nextImage(context)),
               ],
@@ -41,31 +76,16 @@ class ShowImage extends StatelessWidget {
   }
 
   prevImagee(BuildContext context) {
-    int prevIndex = index - 1;
-    Navigator.pop(context);
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => ShowImage(
-                  prevIndex,
-                  urls: urls,
-                )));
+    animationStart = Offset(1.0, 0.0);
+    setState(() {
+      index -= 1;
+    });
   }
 
   nextImage(BuildContext context) {
-    int prevIndex = index + 1;
-    Navigator.pop(context);
-
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => ShowImage(
-                  prevIndex,
-                  urls: urls,
-                )));
+    animationStart = Offset(-1.0, 0.0);
+    setState(() {
+      index += 1;
+    });
   }
-
-  //TODO: Transition when swiping
-  //TODO: Load 30 pics at once not all of it for optimisation and when scrolled down load the rest and remove prevously one by one
-  //Same for image viewing
 }
--- example/lib/Pages/Profile/tabbody/pics.dart
+++ example/lib/Pages/Profile/tabbody/pics.dart
@@ -18,49 +18,52 @@ class _PictureTabState extends State<PictureTab>
   var color = Colors.black;
   @override
   Widget build(BuildContext context) {
+    //AnimationLimiter is used to animate on build or return on this page
     return AnimationLimiter(
-      child: GridView.count(
-        crossAxisCount: gridCount,
-        crossAxisSpacing: 5,
-        mainAxisSpacing: 5,
-        physics: BouncingScrollPhysics(),
-        children: List.generate(widget.urls.length, (index) {
-          return AnimationConfiguration.staggeredGrid(
-              position: index,
-              columnCount: gridCount,
-              child: ScaleAnimation(
-                  duration: (Duration(milliseconds: 500)),
-                  child: GestureDetector(
-                    onHorizontalDragStart: (details) =>
-                        startX = details.localPosition.dx,
-                    onHorizontalDragUpdate: (details) =>
-                        endX = details.localPosition.dx,
-                    onHorizontalDragEnd: (details) => calculation(details),
-                    child: GestureDetector(
-                      onTap: () => heroTransgender(index, context),
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                            widget.urls[index],
-                            height: double.infinity,
-                            width: double.infinity,
-                            fit: BoxFit.fill,
-                          )),
-                    ),
-                  )));
-        }),
+      //ANIMATED SWITCHER IS USED TO ANIMATED ON STATE CHANGE
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 300),
+        child: GridView.count(
+          key: ValueKey(gridCount),
+          crossAxisCount: gridCount,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          physics: BouncingScrollPhysics(),
+          children: List.generate(widget.urls.length, (index) {
+            return AnimationConfiguration.staggeredGrid(
+                position: index,
+                columnCount: gridCount,
+                child: ScaleAnimation(
+                    duration: (Duration(milliseconds: 500)),
+                    child: AnimatedSwitcher(
+                      duration: Duration(seconds: 10),
+                      child: GestureDetector(
+                        onHorizontalDragStart: (details) =>
+                            startX = details.localPosition.dx,
+                        onHorizontalDragUpdate: (details) =>
+                            endX = details.localPosition.dx,
+                        onHorizontalDragEnd: (details) => calculation(details),
+                        child: GestureDetector(
+                          onTap: () => heroTransgender(index, context),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.network(
+                                widget.urls[index],
+                                height: double.infinity,
+                                width: double.infinity,
+                                fit: BoxFit.fill,
+                              )),
+                        ),
+                      ),
+                    )));
+          }),
+        ),
       ),
     );
   }
 
   heroTransgender(int index, BuildContext context) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => ShowImage(
-                  index,
-                  urls: widget.urls,
-                )));
+    Navigator.push(context, _createRoute(index));
   }
 
   calculation(DragEndDetails details) {
@@ -86,5 +89,33 @@ class _PictureTabState extends State<PictureTab>
         }
       });
     }
+  }
+
+//MaterialRoute give us the default material route. Learn more about custom route
+  Route _createRoute(int index) {
+    /*
+    Working
+    We create our custom navigationRoute to specify the way we want to transition
+    1.we return a pageRouteBuilder.
+    2. TransitionBiulder takes in all the necessary parameters and then
+    we can customise it. But we have to return a transition
+    3.PageBuilder will be responsible for building the page*/
+    return PageRouteBuilder(
+        transitionDuration: Duration(milliseconds: 300),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          animation =
+              CurvedAnimation(parent: animation, curve: Curves.elasticInOut);
+          return ScaleTransition(
+            scale: animation,
+            child: child,
+            alignment: Alignment.center,
+          );
+        },
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return ShowImage(
+            index,
+            urls: widget.urls,
+          );
+        });
   }
 }
